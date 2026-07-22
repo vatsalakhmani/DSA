99513913fc24c67c09ba94bea4b75acf2dec8fed
vatsalakhmani <vatsallakh14@gmail.com>
2026-07-22 23:45:52 +0530

    Time: 0 ms (100%), Space: 8.4 MB (93.31%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    double pow_raise(double x,long n){
-if(n==0) return 1.0;
-if(n==1) return x;
+//     double pow_raise(double x,long n){
+// if(n==0) return 1.0;
+// if(n==1) return x;
 
-     if(n%2==0) return pow_raise(x*x,n/2);
-     else return x*pow_raise(x,n-1);
-    }
+//      if(n%2==0) return pow_raise(x*x,n/2);
+//      else return x*pow_raise(x,n-1);
+//     }
+//     double myPow(double x, int n) {
+//         long m=n;
+//         if(m<0){
+//             x=1/x;
+//             m=-m;
+//         }
+//         return pow_raise(x,m);
+//     }
     double myPow(double x, int n) {
-        long m=n;
-        if(m<0){
-            x=1/x;
-            m=-m;
+        double res = 1;
+        while (n) {
+            if (n % 2) res = n > 0 ? res * x : res / x;
+            x = x * x;
+            n /= 2;
         }
-        return pow_raise(x,m);
+        return res;
     }
 };
