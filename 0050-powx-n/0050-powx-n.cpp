7d12c4124de609b0c87795bff6be7b1daa603517
vatsalakhmani <vatsallakh14@gmail.com>
2026-07-22 23:22:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.59%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double pow_raise(double x,long n){
+if(n==0) return 1.0;
+if(n==1) return x;
+
+     if(n%2==0) return pow_raise(x*x,n/2);
+     else return x*pow_raise(x,n-1);
+    }
+    double myPow(double x, int n) {
+        long m=n;
+        if(m<0){
+            x=1/x;
+            m=-m;
+        }
+        return pow_raise(x,m);
+    }
+};
