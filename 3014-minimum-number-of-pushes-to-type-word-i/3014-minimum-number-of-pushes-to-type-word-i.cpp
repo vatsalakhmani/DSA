3b83fb7e6b6e4097e1cd2ba575400cab27003c81
vatsalakhmani <vatsallakh14@gmail.com>
2026-07-31 23:50:18 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.24%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public :
+    int minimumPushes(string word) {
+        int n = word.length();
+        int pushCnt = 0;
+
+        for(int i = 0; i < n; i++){
+            pushCnt += i / 8 + 1;
+        }
+
+        return pushCnt;
+    }
+};
