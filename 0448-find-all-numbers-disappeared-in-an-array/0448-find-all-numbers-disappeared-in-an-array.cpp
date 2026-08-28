40da37bc6c42e4dbf6c05a15d2f6b608f20c55a6
vatsalakhmani <vatsallakh14@gmail.com>
2026-08-29 04:34:39 +0530

    Time: 47 ms (17.06%), Space: 64.3 MB (16.83%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+        // using hashmap
+
+        // vector<int> result;
+        // unordered_map<int,int> map;
+        // for(auto num :nums){
+        //     map[num]++;
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //    if(map.count(i+1)==0) result.push_back(i+1);
+        // }
+        // return result;
+
+        // using hasharray
+        //   vector<int>hash(nums.size()+1,0);
+        // for(int i=0;i<nums.size();i++){
+        //     hash[nums[i]]++;
+        // }
+        // vector<int> result;
+        // for(int i=1;i<nums.size() + 1;i++){
+        //     if(hash[i] < 1){
+        //         result.push_back(i);
+        //     }
+        // }
+        // return result;
+
+        //using hashset
         vector<int> result;
-        unordered_map<int,int> map;
-        for(auto num :nums){
-            map[num]++;
-        }
-        for(int i=0;i<nums.size();i++){
-           if(map.count(i+1)==0) result.push_back(i+1);
+        unordered_set<int> st(nums.begin(),nums.end());
+        for(int i=1;i<=nums.size();i++){
+            if(st.count(i)==0) result.push_back(i);
         }
         return result;
     }
